aef7bee29b1f893a703fad4eb04f71dc57b0ccdb
AKG007-prog <aniruddhagawali05@gmail.com>
2020-12-31 15:07:09 +0530

    added site and readme

---
+++ README.md
@@ -0,0 +1,21 @@
+
+# Hi, I'm AniruddhaGawali (AKG) <a href="https://www.gautamkrishnar.com/"><img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="40"></a>
+
+<img align='right' src='https://user-images.githubusercontent.com/5713670/87202985-820dcb80-c2b6-11ea-9f56-7ec461c497c3.gif' width="100">
+
+[![github](https://img.shields.io/badge/AniruddhaGawali-171717?style=flat-square&logo=Github&logoColor=color)](https://github.com/AniruddhaGawali)
+[![gmail](https://img.shields.io/badge/aniruddhagawali05@gmail.com-E95439?style=flat-square&logo=Gmail&logoColor=white)](mailto:aniruddhagawali05@gmail.com)
+[![wesite](https://img.shields.io/badge/%20🔖-AKG_Site-slateblue?style=flat-square&labelColor=slateblue)](https://aniruddhagawali.github.io/akg-site/)
+
+**Hi I am Aniruddha Gawali you can call me AKG in short. I love coding and Computers This is my github. I have crated lots of project you can exploer them on my website. My fravotie language is python so I have made lots of of projects on it like AI, Tkinter application and many more**
+
+- ## Languages I Learn
+
+  
+  ![html](https://img.shields.io/badge/-HTML-silver?style=flat-square&logo=html5&logoColor=color)
+  ![css](https://img.shields.io/badge/-CSS-silver?style=flat-square&logo=CSS3&logoColor=blue) ![javascript](https://img.shields.io/badge/-Javascript-silver?style=flat-square&logo=Javascript&logoColor=color) ![python](https://img.shields.io/badge/Python-silver?style=flat-square&logo=python&logoColor=color) 
+  ![C++](https://img.shields.io/badge/-C++-silver?style=flat-square&logo=c%2b%2b&logoColor=color)
+  ![powershell](https://img.shields.io/badge/-Powershell-silver?style=flat-square&logo=powershell&logoColor=color)  ![shell](https://img.shields.io/badge/-Shell-silver?style=flat-square&logo=shell&logoColor=color)
+
+- ## My Github stats
+  <a href="https://aniruddhagawali.github.io/AKG-PROJECTS/"><img height="137.3px" src="https://github-readme-stats.vercel.app/api?username=AniruddhaGawali&hide_title=true&hide_border=true&show_icons=true&include_all_commits=true&count_private=true&line_height=21&text_color=000&icon_color=000&bg_color=0,ea6161,ffc64d,fffc4d,52fa5a&theme=graywhite" /><img height="137.3px" src="https://github-readme-stats.vercel.app/api/top-langs/?username=AniruddhaGawali&hide=html&hide_title=true&hide_border=true&layout=compact&langs_count=7&exclude_repo=comp426&text_color=000&icon_color=fff&bg_color=0,52fa5a,4dfcff,c64dff&theme=graywhite"/></a>
---
+++ js/utili.js
@@ -0,0 +1,94 @@
+let up_arrow = "https://i.ibb.co/NsMtrZw/up-arrow.png";
+let down_arrow = "https://i.ibb.co/fFdYGWQ/angle-arrow-down.png";
+function menu() {
+  var x = document.getElementById("nav");
+  if (x.style.display === "none") {
+    x.style.display = "flex";
+    document.getElementById("menubtn").src = up_arrow;
+  } else {
+    x.style.display = "none";
+    document.getElementById("menubtn").src = down_arrow;
+  }
+}
+
+function change_arrow() {
+  var x = document.getElementById("nav");
+  if (x.style.display === "none") {
+    document.getElementById("menubtn").src = down_arrow;
+  } else if (x.style.display === "flex") {
+    document.getElementById("menubtn").src = up_arrow;
+  } else {
+    document.getElementById("menubtn").src = down_arrow;
+  }
+}
+
+let themes = 0;
+function theme() {
+  if (themes === 0) {
+    document.body.style.backgroundColor = "rgb(14,14,14)";
+    document.documentElement.style.setProperty("--text_color", "#fff");
+    document.documentElement.style.setProperty("--text_color2", "#000");
+    document.documentElement.style.setProperty("--nav_color", "rgb(14,14,14)");
+    document.documentElement.style.setProperty("--scroll-color", " #f1f1f1");
+    
+    document.getElementById("theme").src =
+      "https://i.ibb.co/QFpDYhr/theme-l.png";
+    document.getElementById("theme").style.transform = "rotate(360deg)";
+    // document.getElementById("theme").style.transition = 'transform 1s ,margin-bottom .25s';
+
+    down_arrow = "https://i.ibb.co/cyfzHDW/imageonline-co-invertedimage.png";
+    up_arrow = "https://i.ibb.co/1zzPK5V/imageonline-co-invertedimage-1.png";
+    change_arrow();
+    themes = 1;
+  } else {
+    document.body.style.backgroundColor = "white";
+    document.documentElement.style.setProperty("--text_color", "#000");
+    document.documentElement.style.setProperty("--text_color2", "#fff");
+    document.documentElement.style.setProperty("--nav_color", "#fff");
+    document.documentElement.style.setProperty("--scroll-color", "rgb(155, 155, 155)");
+
+    document.getElementById("theme").src =
+      "https://i.ibb.co/zrK9B08/theme-d.png";
+    document.getElementById("theme").style.transform = "rotate(0deg)";
+    // document.getElementById("theme").style.transition = 'transform 1s,margin-bottom .25s';
+
+    up_arrow = "https://i.ibb.co/NsMtrZw/up-arrow.png";
+    down_arrow = "https://i.ibb.co/fFdYGWQ/angle-arrow-down.png";
+    change_arrow();
+    themes = 0;
+  }
+}
+
+let old_id = '2003'
+let old_color = '1'
+function display_on(id,color) { 
+  if (document.getElementById(old_id).style.display == 'block'){
+    document.getElementById(old_id).style.display = 'none'
+    document.getElementById(old_color).style.background='transparent'
+    
+  }
+  document.getElementById(id).style.display='block'
+  document.getElementById(color).style.background='var(--maincolor)'
+
+  old_id = id
+  old_color= color
+  
+}
+
+
+const userPrefersDark =
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+const userPrefersLight =
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: light)").matches;
+
+if (userPrefersDark) {
+  theme();
+}
+
+document.getElementById('2003').style.display='block'
+
+
+
