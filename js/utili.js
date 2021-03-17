a862b3becff9524af258f850fb62e3f8f919fe44
Aniruddha Gawali <aniruddhagawali05@gmail.com>
2021-03-17 23:11:41 +0530

    mnor srollbar changes

--- js/utili.js
+++ js/utili.js
@@ -8,7 +8,7 @@ function theme() {
     document.documentElement.style.setProperty("--text_color2", "#000");
     document.documentElement.style.setProperty("--nav_color", "rgb(14,14,14)");
     document.documentElement.style.setProperty("--icon", "rgb(20,20,20)");
-    document.documentElement.style.setProperty("--scroll-color", " #f1f1f1");
+    document.documentElement.style.setProperty("--scroll-color", " #333");
 
     document.getElementById("theme").src =
       "https://i.ibb.co/QFpDYhr/theme-l.png";
@@ -24,7 +24,7 @@ function theme() {
     document.documentElement.style.setProperty("--text_color2", "#fff");
     document.documentElement.style.setProperty("--nav_color", "#fff");
     document.documentElement.style.setProperty("--icon", "#fff");
-    document.documentElement.style.setProperty("--scroll-color", "rgb(155, 155, 155)");
+    document.documentElement.style.setProperty("--scroll-color", "#cecece");
 
     document.getElementById("theme").src =
       "https://i.ibb.co/zrK9B08/theme-d.png";
